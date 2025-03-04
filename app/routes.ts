52827d2cad0afefe8196f37a004233a0e3db3834
aticcmahbub <aticcmahbub@gmail.com>
2025-03-04 23:33:05 +0600

    first commit

---
+++ app/routes.ts
@@ -0,0 +1,9 @@
+import {index, layout, route, type RouteConfig} from '@react-router/dev/routes';
+
+export default [
+    layout('layouts/sidebar.tsx', [
+        index('routes/home.tsx'),
+        route('contacts/:contactId', 'routes/contacts.tsx'),
+    ]),
+    route('about', 'routes/about.tsx'),
+] satisfies RouteConfig;
